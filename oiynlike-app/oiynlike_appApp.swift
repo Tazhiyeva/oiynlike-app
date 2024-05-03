b7072deba854d59a0fe90073659d24e542297932
Madina <madinatazhiyeva@gmail.com>
2024-05-03 14:32:09 +0600

    Initial Commit

---
+++ oiynlike-app/oiynlike_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  oiynlike_appApp.swift
+//  oiynlike-app
+//
+//  Created by Madina Tazhiyeva on 03.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct oiynlike_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
